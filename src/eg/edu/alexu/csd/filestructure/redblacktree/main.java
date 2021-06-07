84831e5de973601a87ea5c1211072ffac92f3e22
Mariemyoussri <61235438+Mariemyoussri@users.noreply.github.com>
2021-06-07 20:30:41 +0200

    . predecessor and successor added

---
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/main.java
@@ -0,0 +1,35 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class main {
+
+    public static void main(String[] args){
+        /*RedBlackTree<Integer, Integer> tree = new RedBlackTree<Integer, Integer>();
+        tree.insert(33,33);
+        tree.insert(13,13);
+        tree.insert(53,53);
+        tree.insert(11,11);
+        tree.insert(21,21);
+        tree.insert(41,41);
+        tree.insert(61,61);
+        tree.insert(15,15);
+        tree.insert(31,31);
+        System.out.println(tree.search(33));
+        System.out.println(tree.contains(41));
+
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        map.put();
+
+         */
+        /*RedBlackTree<Integer, Integer> tree = new RedBlackTree<Integer, Integer>();
+        tree.insert(33,33);
+        tree.insert(13,13);
+        tree.insert(53,53);
+        tree.leftRotation(tree.searchForNode(tree.getRoot(),33));
+        System.out.println("done");
+
+         */
+    }
+}
